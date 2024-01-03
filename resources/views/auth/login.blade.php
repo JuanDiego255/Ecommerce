b76562305026f040e4e6962d53b16abec9ec85a6
JuanDiego255 <jastorgalopez6@gmail.com>
2024-01-03 12:06:24 -0600

    Cambio de estilo login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,63 +4,81 @@
     {!! OpenGraph::generate() !!}
 @endsection
 @section('content')
-    <div class="container mt-4">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">{{ __('Iniciar Sesión') }}</div>
+    <section class="text-center text-lg-start">
+        <style>
+            .cascading-right {
+                margin-right: -50px;
+            }
 
-                    <div class="card-body">
-                        <form method="POST" action="{{ route('login') }}">
-                            @csrf
+            @media (max-width: 991.98px) {
+                .cascading-right {
+                    margin-right: 0;
+                }
+            }
+        </style>
 
-                            <div class="col-md-12 mt-2">
-                                <div class="input-group input-group-static mb-4">
-                                    <label>E-mail</label>
-                                    <input value="{{ old('email') }}" autocomplete="email" required type="email"
-                                        name="email" class="form-control float-left w-100">
+        <!-- Jumbotron -->
+        <div class="container py-4">
+            <div class="row g-0 align-items-center">
+                <div class="col-lg-6 mb-5 mb-lg-0">
+                    <div class="card cascading-right card-login">
+                        <div class="card-body p-5 shadow-5 text-center">
+                            <h4 class="fw-bold mb-5">Ingresa ahora</h4>
+                            <div class="card-body">
+                                <form method="POST" action="{{ route('login') }}">
+                                    @csrf
+
+                                    <div class="col-md-12">
+                                        <div class="input-group input-group-static mb-4">
+                                            <label>E-mail</label>
+                                            <input value="{{ old('email') }}" autocomplete="email" required type="email"
+                                                name="email" class="form-control float-left w-100">
+                                        </div>
+                                    </div>
+
+                                    <div class="col-md-12 mt-2">
+                                        <div class="input-group input-group-static mb-4">
+                                            <label>Clave</label>
+                                            <input value="{{ old('password') }}" required autocomplete="current-password"
+                                                type="password" name="password" class="form-control float-left w-100">
+                                        </div>
+                                    </div>
+
+                                    <div class="row mb-0">
+                                        <div class="col-md-12">
+                                            <center>
+                                                <button type="submit" class="btn btn-info">
+                                                    {{ __('Ingresar') }}
+                                                </button>
+                                            </center>
+                                        </div>
+                                    </div>
+
+                                </form>
+                                <div class="divider d-flex align-items-center">
+                                    <p class="text-center fw-bold mx-3 text-muted">O INGRESA CON:</p>
                                 </div>
-                            </div>
+                                <div class="text-center">
+                                    <a href="{{ url('/facebook-auth/redirect') }}" class="m-5 text-facebook">
+                                        <i class="fa fa-facebook"></i>
+                                    </a>
 
-                            <div class="col-md-12 mt-2">
-                                <div class="input-group input-group-static mb-4">
-                                    <label>Clave</label>
-                                    <input value="{{ old('password') }}" required autocomplete="current-password"
-                                        type="password" name="password" class="form-control float-left w-100">
+                                    <a href="{{ url('/google-auth/redirect') }}" class="m-5 text-google">
+                                        <i class="fa fa-google"></i>
+                                    </a>
                                 </div>
-                            </div>
 
-                            <div class="row mb-0">
-                                <div class="col-md-12">
-                                    <center>
-                                        <button type="submit" class="btn btn-info">
-                                            {{ __('Ingresar') }}
-                                        </button>
-                                    </center>
-                                </div>
                             </div>
-                        </form>
+                        </div>
                     </div>
+                </div>
+
+                <div class="col-lg-6 mb-5 mb-lg-0">
+                    <img src="{{url('images/login.PNG');}}" class="w-100 rounded-4 shadow-4"
+                        alt="" />
                 </div>
             </div>
         </div>
-
-        <center>
-            <div class="mt-3 w-75">
-
-                <a class="btn btn-google w-75 text-center" href="{{ url('/google-auth/redirect') }}">
-                    <i class="fa fa-google"></i> Ingresar con Google
-                </a>
-
-
-                <a class="btn btn-facebook w-75 text-center" href="{{ url('/facebook-auth/redirect') }}">
-                    <i class="fa fa-facebook"></i> Ingresar con Facebook
-                </a>
-
-            </div>
-
-        </center>
-
-
-    </div>
+        <!-- Jumbotron -->
+    </section>
 @endsection
