3434b94808dd7bd8072a8dbe82e528bb898ed110
JuanDiego255 <jastorgalopez6@gmail.com>
2025-04-24 12:02:21 -0600

    Se crea funcionalidad distinta para Aclimate

--- resources/views/frontend/design_ecommerce/index.blade.php
+++ resources/views/frontend/design_ecommerce/index.blade.php
@@ -80,7 +80,7 @@
                                         <a href="{{ url(($prefix == 'aclimate' ? $prefix . '/' : '') . 'clothes-category/' . $item->category_id . '/' . $item->department_id) }}"
                                             class="block1-txt ab-t-l s-full flex-col-l-sb p-lr-38 p-tb-34 trans-03 respon3">
                                             <div class="block1-txt-child1 flex-col-l">
-                                                <span class="block1-name ltext-102 trans-04 p-b-8">
+                                                <span class="ltext-102 trans-04 p-b-8 {{isset($tenantinfo->tenant) && $tenantinfo->tenant != 'aclimate' ? 'block1-name' : 'block1-name-ac'}}">
                                                     {{ $item->name }}
                                                 </span>
 
@@ -230,7 +230,7 @@
                     </button>
                     <input class="mtext-107 cl2 size-114 plh2 p-r-15" type="text" name="searchfor" id="searchfor"
                         placeholder="Buscar">
-                </div>              
+                </div>
             </div>
             <!-- Aquí podrías agregar un panel-filter similar si lo requieres -->
 
